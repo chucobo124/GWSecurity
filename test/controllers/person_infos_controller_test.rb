00f0afaf188834ae21ca168ccdd6f6bc99c0b90b
Shao Yu Lung <bestlong168@gmail.com>
2015-06-15 00:06:05 +0800

    This resolved #22, 測試錯誤：解決與授權相關的部分。

--- test/controllers/person_infos_controller_test.rb
+++ test/controllers/person_infos_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class PersonInfosControllerTest < ActionController::TestCase
   test "should get index" do
+    sign_in users(:user1)
     get :index
     assert_response :success
   end
