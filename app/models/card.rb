0ab264a682adceefdbc9b74c4403ec9093e6e651
Shao Yu Lung <bestlong168@gmail.com>
2015-06-14 22:26:24 +0800

    解決測試失敗，在 fixtures 設定的部分。

---
+++ app/models/card.rb
@@ -0,0 +1,5 @@
+class Card < ActiveRecord::Base
+
+  belongs_to :person_info
+
+end
--- app/models/person_info.rb
+++ app/models/person_info.rb
@@ -1,5 +1,6 @@
 class PersonInfo < ActiveRecord::Base
 	belongs_to :user
+  has_many :cards
 	def self.myacount(userID)
 	  self.where(userId: userID).take
 	end
