d754aa77b250aa3284afb7fbda29f4d565d2fe90
Shao Yu Lung <bestlong168@gmail.com>
2015-06-16 21:53:21 +0800

    imporve test.

--- test/controllers/sensors_controller_test.rb
+++ test/controllers/sensors_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class SensorsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "no login should get index then redirect 302" do
+    get :index
+    assert_response :redirect
+  end
+
+  test "after login should get index" do
+    sign_in users(:user1)
+    get :index
+    assert_response :success
+  end
 end
--- test/models/sensor_log_test.rb
+++ test/models/sensor_log_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class SensorLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @sensor = Sensor.new(
+      :id => 1, 
+      :name => 'Main Power', 
+      :api_key => '1234567890123456',
+      :v1_name => 'AC 110',
+      :v1_unit => 'V',
+      :v2_name => 'AC 220',
+      :v2_unit => 'V'
+      )
+    @sensor.save
+
+    @sensor_log = @sensor.sensor_logs.new
+    @sensor_log.v1 = '111'
+    @sensor_log.v2 = '222'
+  end
+
+  test "valid model" do
+    assert @sensor_log.save
+  end
+
 end
