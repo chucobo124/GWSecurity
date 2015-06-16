42f8db0c1df700fd0e0b1a04d71c25974ec48124
Shao Yu Lung <bestlong168@gmail.com>
2015-06-16 21:41:06 +0800

    imporve sensor model test.

--- test/models/sensor_test.rb
+++ test/models/sensor_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class SensorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @sensor = Sensor.new(:id => 1, :name => 'Main Power', :api_key => '1234567890123456')
+  end
+
+  test "valid model" do
+    assert @sensor.save
+  end
+
+  test "require name" do
+    @sensor.name = ''
+    assert_not @sensor.save
+    @sensor.name = nil
+    assert_not @sensor.save
+  end
+
+  test "require api_key" do
+    @sensor.api_key = ''
+    assert_not @sensor.save
+    @sensor.api_key = nil
+    assert_not @sensor.save
+  end
+
 end
